ed5a2c2f8a074a9ba93e3dfb82df6eef9cc96272
Tulsi <tulsi.gautam0000@gmail.com>
2025-01-07 18:28:50 +0000

    more mobile responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,16 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
+		<div
+			className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'
+		>
+			<Sidebar className="w-full sm:w-1/3" />
+			<MessageContainer className="w-full h-[calc(100vh-1rem)] sm:w-full sm:h-full overflow-auto" />
 		</div>
 	);
 };
+
 export default Home;
+
+
+
